6988a280b2ac2fa9fd7851733a9b057ea73c249e
deokk <ckdejr5352@naver.com>
2025-12-22 18:45:07 +0900

    완료

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,3 @@
-from flask import Flask
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # .env 파일 로드
-
-api_key = os.getenv("SEOUL_API_KEY")
-db_user = os.getenv("DB_USER")
-
-print(f"API KEY: {api_key}")
-print(f"DB USER: {db_user}")
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Hello, SMC Backend! 🚲'
-
-if __name__ == '__main__':
-    app.run(debug=True)
 from dotenv import load_dotenv
 load_dotenv() # .env 파일에서 환경 변수를 로드합니다.
 
